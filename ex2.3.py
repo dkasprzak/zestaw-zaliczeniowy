8d86fa69f053a9da156795a83a1c1841e223b803
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-05-27 11:50:06 +0200

    Za pomocą API zwracającego informacje dot. uniwersytetów w danym państwie:  http://universities.hipolabs.com/search?country=<nazwa_kraju_eng>  wyświetl nazwy uniwersytetów z 15 wybranych krajów w postaci:  {<nazwa_kraju>: [<nazwa_uniwersytet1>, <nazwa_uniwersytet2>,...], …}.  W celu przyspieszenia pobierania danych, wykorzystaj moduł threading do realizacji  wielowątkowego pobierania informacji

---
+++ ex2.3.py
@@ -0,0 +1,29 @@
+import threading
+import requests
+
+# Lista 15 wybranych krajów
+countries = ["Poland", "Germany", "Italy", "France", "Spain", "Switzerland", "Austria", "Belgium",
+             "Canada", "India", "China", "San Marino", "Japan", "Australia", "United Kingdom"]
+
+universities_by_country = {}
+
+def fetch_universities(country):
+    response = requests.get(f"http://universities.hipolabs.com/search?country={country}")
+    if response.status_code == 200:
+        data = response.json()
+        universities = [university['name'] for university in data]
+        universities_by_country[country] = universities
+    else:
+        universities_by_country[country] = []
+
+threads = []
+
+for country in countries:
+    thread = threading.Thread(target=fetch_universities, args=(country,))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+print(universities_by_country)
